2608de94978e11e6a56b37f03bb56af1566b4562
jorgegranados45 <jorgegranados45@gmail.com>
2021-07-01 18:30:28 +0200

    Changing App to GIfApp

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import GifApp from "./GifApp";
+
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GifApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
