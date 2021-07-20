b463d0ab662c7459a5e3e1de894bac4d93402152
jorgegranados45 <jorgegranados45@gmail.com>
2021-07-20 15:47:54 +0200

    fix: The way of passing the parameters is changed to the correct way.

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -14,7 +14,8 @@ export const GifGrid = ({ category, setLoading }) => {
             {setLoading(loading)}
             {
                 data.map( image => {
-                    return <GifGridItem key={image.id} image={image} />
+                    //When you need to pass a lot of parameters, you should use the spread operator without variable assignment
+                    return <GifGridItem key={image.id} {...image} />
                 })
             }
         </>
