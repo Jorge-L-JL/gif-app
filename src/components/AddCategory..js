a98308627926b807bf356f4ef27f148b4286af36
jorgegranados45 <jorgegranados45@gmail.com>
2021-07-20 16:26:56 +0200

    feat: categories propTpye is added

--- src/components/AddCategory..js
+++ src/components/AddCategory..js
@@ -58,5 +58,6 @@ export const AddCategory = ({setCategories, categories}) => {
 }
 
 AddCategory.propTypes = {
-    setCategories: PropTypes.func.isRequired
+    setCategories: PropTypes.func.isRequired,
+    categories: PropTypes.array.isRequired
 }
