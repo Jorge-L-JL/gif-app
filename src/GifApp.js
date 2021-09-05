8428b710fa08e083ba9ce71e5bc55bb17c8de821
jorgegranados45 <jorgegranados45@gmail.com>
2021-09-05 16:02:50 +0200

    fix: The import of AddCategory is modified

--- src/GifApp.js
+++ src/GifApp.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {AddCategory} from "./components/AddCategory.";
+import {AddCategory} from "./components/AddCategory";
 import {GifGrid} from "./components/GifGrid";
 import { makeStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress'
